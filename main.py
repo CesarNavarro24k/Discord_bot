e2d77796e96f62cbc380fbafe35cb8eb43ae64fa
CesarNavarro24k <138552856+CesarNavarro24k@users.noreply.github.com>
2024-12-26 17:21:05 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord 
+from discord.ext import commands
+import os
+import webserver
+import random
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Bot is ready')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+@bot.command()
+async def suma(ctx,n1:int,n2:int):
+    """Esta función suma dos números enteros y devuelve el resultado."""
+    resultado = n1 + n2
+    await ctx.send(f"La suma de {n1} y {n2} es {resultado}")
+webserver.keep_alive()
+bot.run(DISCORD_TOKEN)
